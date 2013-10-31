e3aa2998fd11f9be28ba15bc006b450d70c516bc
David Winterbottom <david.winterbottom@gmail.com>
2013-10-31 10:11:12 +0000

    Submit order to Avalara after submission

--- sandbox/apps/checkout/views.py
+++ sandbox/apps/checkout/views.py
@@ -1,13 +1,20 @@
+import logging
+
 from oscar.apps.checkout import views
 from django.contrib import messages
 from django.template import loader
 
 import avalara
 
+logger = logging.getLogger('avalara')
+
 
 class PaymentDetailsView(views.PaymentDetailsView):
 
     def build_submission(self, **kwargs):
+        # Override the build_submission method so we can apply taxes to the
+        # submission dict
+
         submission = super(PaymentDetailsView, self).build_submission(**kwargs)
 
         # Fetch and apply taxes to submission dict
@@ -20,3 +27,21 @@ class PaymentDetailsView(views.PaymentDetailsView):
             messages.error(self.request, msg, extra_tags="safe noicon")
 
         return submission
+
+    def handle_successful_order(self, order):
+        # Override the handle_successful_order method so we can submit the
+        # order to Avalara for tax reporting
+
+        response = super(PaymentDetailsView, self).handle_successful_order(
+            order)
+
+        # Submit tax information to Avalara
+        try:
+            avalara.submit(order)
+        except Exception:
+            # Tax can be re-submitted later so we swallow all possible
+            # exceptions and log them.
+            logger.error("Unable to submit tax information for order %s",
+                         order.number, exc_info=True)
+
+        return response
